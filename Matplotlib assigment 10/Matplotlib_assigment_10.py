a1f23d14194138bf5f1c89fad423769eae584d19
EXVLO <nadirashvilisandro888@gmail.com>
2025-01-23 07:24:50 +0400

    First Step: Plot functions on graph

---
+++ Matplotlib assigment 10/Matplotlib_assigment_10.py
@@ -0,0 +1,17 @@
+import random
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(-10, 10, 1000)
+
+f1 = x**2
+f2 = x * np.sin(2 * x)
+f3 = np.arctan(x)
+
+plt.figure(figsize = (10, 6))
+plt.plot(x, f1, label = r"$f(x) = x^2$", color = "blue")
+plt.plot(x, f2, label = r"$f(x) = x \sin(2x)$", color = "green")
+plt.plot(x, f3, label = r"$f(x) = \arctan(x)$", color = "red")
+
+plt.show()
