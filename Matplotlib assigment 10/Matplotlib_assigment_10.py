488502b66d95d5216447ef9d1aab1b55a39d434b
EXVLO <nadirashvilisandro888@gmail.com>
2025-01-23 07:26:45 +0400

    Second Step: Add visuals to graph

--- Matplotlib assigment 10/Matplotlib_assigment_10.py
+++ Matplotlib assigment 10/Matplotlib_assigment_10.py
@@ -14,4 +14,12 @@ plt.plot(x, f1, label = r"$f(x) = x^2$", color = "blue")
 plt.plot(x, f2, label = r"$f(x) = x \sin(2x)$", color = "green")
 plt.plot(x, f3, label = r"$f(x) = \arctan(x)$", color = "red")
 
+plt.title("Graphs")
+plt.xlabel("x")
+plt.ylabel("f(x)")
+plt.axhline(0, color = "black", linewidth = 1, linestyle = "-")
+plt.axvline(0, color = "black", linewidth = 1, linestyle = "-")
+plt.grid(True, linestyle = "--", alpha = 0.6)
+plt.legend()
+
 plt.show()
